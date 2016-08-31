7eee1fd63d95f661f3e0fffc53067278a7e2369a
Andrew Dirksen <andrew@dirksen.com>
2016-08-30 23:24:48 -0700

    Ground for inertial reference.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const th = require('three')
-  ,[scene, camera] = require('./boiler.js')()
-  ,jpc = require('./three_jetpack_controls.js')
+const th = require('three'),
+    [scene, camera] = require('./boiler.js')(),
+    jpc = require('./three_jetpack_controls.js')
 
 const box = new th.Mesh(new th.BoxGeometry(200, 200, 200), new th.MeshBasicMaterial({
     color: 0xff0000,
@@ -11,6 +11,19 @@ setInterval(() => {
     box.position.z -= 1
 })
 
-scene.add(box);
+scene.add(box)
+
+{
+    const p = new th.PlaneGeometry(2000, 2000, 5, 5)
+    p.applyMatrix(new th.Matrix4().makeRotationX(-Math.PI / 2))
+    new th.TextureLoader().load('vesta.jpg', t => {
+        t.wrapS = t.wrapT = th.RepeatWrapping
+        t.repeat.set(32, 32)
+        material = new th.MeshBasicMaterial({
+            map: t
+        })
+        scene.add(new th.Mesh(p, material))
+    })
+}
 
 jpc(scene, camera)
