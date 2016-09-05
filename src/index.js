32c7c490221f9fe345e865dd8748299c10b3097a
Andrew Dirksen <andrew@dirksen.com>
2016-09-04 19:54:44 -0700

    Use pointerlock polyfil

--- src/index.js
+++ src/index.js
@@ -1,29 +1,30 @@
 const th = require('three'),
-    [scene, camera] = require('./boiler.js')(),
-    jpc = require('./three_jetpack_controls.js')
+  [scene, camera] = require('./boiler.js')(),
+  jpc = require('./three_jetpack_controls.js')
 
 const box = new th.Mesh(new th.BoxGeometry(200, 200, 200), new th.MeshBasicMaterial({
-    color: 0xff0000,
-    wireframe: true
+  color: 0xff0000,
+  wireframe: true
 }))
 
 setInterval(() => {
-    box.position.z -= 1
+  box.position.z -= 1
+  box.position.y += 0.1
 })
 
 scene.add(box)
 
 {
-    const p = new th.PlaneGeometry(2000, 2000, 5, 5)
-    p.applyMatrix(new th.Matrix4().makeRotationX(-Math.PI / 2))
-    new th.TextureLoader().load('vesta.jpg', t => {
-        t.wrapS = t.wrapT = th.RepeatWrapping
-        t.repeat.set(32, 32)
-        material = new th.MeshBasicMaterial({
-            map: t
-        })
-        scene.add(new th.Mesh(p, material))
+  const p = new th.PlaneGeometry(2000, 2000, 5, 5)
+  p.applyMatrix(new th.Matrix4().makeRotationX(-Math.PI / 2))
+  new th.TextureLoader().load('vesta.jpg', t => {
+    t.wrapS = t.wrapT = th.RepeatWrapping
+    t.repeat.set(32, 32)
+    material = new th.MeshBasicMaterial({
+      map: t
     })
+    scene.add(new th.Mesh(p, material))
+  })
 }
 
 jpc(scene, camera)
--- src/three_jetpack_controls.js
+++ src/three_jetpack_controls.js
@@ -1,35 +1,29 @@
 const th = require('three')
-const full_screen = require('screenfull')
-
-const lock_pointer = on_change => {
-    document.addEventListener('pointerlockchange',
-        e => on_change(!!document.pointerLockElement))
-
-    document.body.addEventListener("click", () => {
-        document.body.requestPointerLock()
-        full_screen.request(document.body)
-    })
-}
+const screenfull = require('screenfull')
+const pointer_lock = require('pointer-lock')
 
 module.exports = (scene, camera) => {
-    const pitch = new th.Object3D()
-    pitch.add(camera)
+  const pitch = new th.Object3D()
+  pitch.add(camera)
 
-    const yaw = new th.Object3D()
-    yaw.position.y = 10
-    yaw.add(pitch)
+  const yaw = new th.Object3D()
+  yaw.position.y = 10
+  yaw.add(pitch)
 
-    scene.add(yaw)
+  scene.add(yaw)
 
-    const PI_2 = Math.PI / 2
+  const PI_2 = Math.PI / 2
 
-    const onmousemove = event => {
-        yaw.rotation.y -= event.movementX * 0.002
-        pitch.rotation.x -= event.movementY * 0.002
-        pitch.rotation.x = Math.max(-PI_2, Math.min(PI_2, pitch.rotation.x))
-    }
+  const onmousemove = event => {
+    yaw.rotation.y -= event.movementX * 0.002
+    pitch.rotation.x -= event.movementY * 0.002
+    pitch.rotation.x = Math.max(-PI_2, Math.min(PI_2, pitch.rotation.x))
+  }
 
-    lock_pointer(lock => lock ?
-        document.addEventListener('mousemove', onmousemove) :
-        document.removeEventListener('mousemove', onmousemove))
+  document.body.addEventListener("click", () => {
+    const p = pointer_lock(document.body)
+    p.on('attain', () => document.addEventListener('mousemove', onmousemove))
+    p.on('release', () => document.removeEventListener('mousemove', onmousemove))
+  })
+
 }
