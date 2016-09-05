deee68ef365f7477e6cce7620b81d8da5b2f371d
Andrew Dirksen <andrew@dirksen.com>
2016-09-04 18:52:32 -0700

    gulp with auto browserify and live reload

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var browser_sync = require('browser-sync').create();
+var source = require('vinyl-source-stream')
+var browserify = require('browserify')
+var gulp = require('gulp')
+
+function start_server() {
+  browser_sync.init({
+    server: {
+      baseDir: "static"
+    }
+  })
+}
+
+function on_src_update() {
+  browserify('./src/index.js').bundle()
+    .pipe(source('index.js'))
+    .pipe(gulp.dest('./static'))
+
+  browser_sync.reload()
+}
+
+gulp.task('watch', () => {
+  start_server()
+  gulp.watch('src/*', on_src_update)
+})
